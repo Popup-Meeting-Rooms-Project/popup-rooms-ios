6d43ff33c46763bff6164f900883a10c9894efe5
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2021-11-30 10:34:27 +0200

    Added swipe down to refresh.

--- PopUpRooms/Views/Rooms/RoomList.swift
+++ PopUpRooms/Views/Rooms/RoomList.swift
@@ -50,37 +50,68 @@ struct RoomList: View {
         }
     }
     
-    
     var body: some View {
         NavigationView {
-            List {
-                Toggle(isOn: $showFavsOnly) {
-                    Text("Show Favorites only")
-                }
-                Toggle(isOn: $showAvailableOnly) {
-                    Text("Show Available only")
-                }
-                
-                Section {
-                    //RoomRow(room: Room(id:"a3",roomNo:101,floor:1,status:true))   USED FOR DEBUGGING
-                    ForEach(filteredRooms) { room in
-                        RoomRow(room: room)
+            if #available(iOS 15.0, *) {
+                List {
+                    Toggle(isOn: $showFavsOnly) {
+                        Text("Show Favorites only")
                     }
-                } header: {
-                    ListHeader()
+                    Toggle(isOn: $showAvailableOnly) {
+                        Text("Show Available only")
+                    }
+                    
+                    Section {
+                        ForEach(filteredRooms) { room in
+                            RoomRow(room: room)
+                        }
+                    } header: {
+                        ListHeader()
+                    }
+                    
                 }
+                .padding(.top, 20).padding(.bottom, 20)
+                // We change the style of the list to fit better our sketches.
+                .listStyle(.inset)
+                
+                // This adds an animation for the expanding/collapsing of the list.
+                .animation(.spring())
+                
+                // This allows pulldown to refresh
+                .refreshable { roomData.load()}
+                
+                // Here we set the title (and styling).
+                .navigationTitle("Main Office")
+                
+            } else {  // Older versions of iOS do not support refreshable. Fallback to original implementation.
+                List {
+                    Toggle(isOn: $showFavsOnly) {
+                        Text("Show Favorites only")
+                    }
+                    Toggle(isOn: $showAvailableOnly) {
+                        Text("Show Available only")
+                    }
+                    
+                    Section {
+                        ForEach(filteredRooms) { room in
+                            RoomRow(room: room)
+                        }
+                    } header: {
+                        ListHeader()
+                    }
+                    
+                }
+                .padding(.top, 20).padding(.bottom, 20)
+                // We change the style of the list to fit better our sketches.
+                .listStyle(.inset)
+                
+                // This adds an animation for the expanding/collapsing of the list.
+                .animation(.spring())
+                
+                // Here we set the title (and styling).
+                .navigationTitle("Main Office")
                 
             }
-            .padding(.top, 20).padding(.bottom, 20)
-            // We change the style of the list to fit better our sketches.
-            .listStyle(.inset)
-            
-            // This adds an animation for the expanding/collapsing of the list.
-            .animation(.spring())
-            
-            // Here we set the title (and styling).
-            .navigationTitle("Main Office")
-            
         }
         // Fixes some warnings on XCode (Apple bug?).
         .navigationViewStyle(.stack)
